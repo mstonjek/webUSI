e0d17a442709f7adc38dcd5ea3c0e4358b3dd931
mstonjek <business@mstonjek.cz>
2024-04-29 10:04:37 +0200

    Database config

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+    config = [
+        'db_host' => 'localhost',
+        'db_name' => 'your_database_name',
+        'db_user' => 'your_username',
+        'db_pass' => 'your_password'
+    ];
