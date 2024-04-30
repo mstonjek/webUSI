ad2f867f477d3b0cc685c0ac108b74103edf5a59
stefccc <stefan353sr@gmail.com>
2024-04-30 09:21:00 +0200

    presununuty <main> do php souboru

--- pages/officialBoard.php
+++ pages/officialBoard.php
@@ -1,6 +1,14 @@
 <?php
     include_once("../includes/header.php");
 ?>
-
+<main class="uredni-deska">
+        <ul>
+            <li><a href="">Podmínky přijetí do UŠI</a></li>
+            <li><a href="">Stanovy UŠI</a></li>
+            <li><a href="">Formulář ke stažení</a></li>
+            <li><a href="">Logomanuál UŠI</a></li>
+            <li><a href="">Průvodce</a></li>
+        </ul>
+    </main>
 <?php
     include_once("../includes/footer.php");
--- pages/signup.php
+++ pages/signup.php
@@ -2,6 +2,21 @@
     include_once("../includes/header.php");
 
 ?>
-
+<main class="chci-se-pripojit">
+        <div class="navodNaPripojeni">
+            <h1>Návod na vyplnění</h1>
+            <ul>
+                <li>Bod 1</li>
+                <li>Bod 2</li>
+            </ul>
+        </div>
+        <a href="..">
+            <a target="_blank" href="../templates/assets/Podminky_pro_prijeti.pdf">
+                <div class="button">
+                    <p>Stáhnout podmínky</p>
+                </div>
+            </a>
+        </a>
+    </main>
 <?php
     include_once("../includes/footer.php");
